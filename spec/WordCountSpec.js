d6c40b9b449378527b59404f35de9fce2ffb6107
Colin Turner <colinturner34@gmail.com>
2017-06-14 00:18:57 +0100

    Add failing test for WordCount dictionary

--- spec/WordCountSpec.js
+++ spec/WordCountSpec.js
@@ -7,9 +7,17 @@ describe("WordCount", function() {
     words = new WordCount("This is a string; great, stuff");
   });
 
-  describe("#split", function(){
-    it("separates a string into an array of words", function(){
-      expect(words.split()).toEqual(["This", "is", "a", "string", "great", "stuff"])
+  describe("#split", function() {
+    it("separates a string into an array of words", function() {
+      expect(words.split()).toEqual(["This", "is", "a", "string", "great", "stuff"]);
+    });
+  });
+
+  describe("#createDictionary", function() {
+    it("creates dictionary with words and their counts", function() {
+      words.split();
+      words.createDictionary();
+      expect(words.dict).toEqual({"this": 1, "is": 1, "a": 1, "string": 1, "great": 1, "stuff": 1});
     });
   });
 
